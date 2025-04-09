b2a202caff80758575e2dcb59f7404abf4345ebc
vdnvarma <dedeepn23@gmail.com>
2025-04-09 19:23:03 +0530

    added websockets

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ connect();
 const app = express();
 
 const corsOptions = {
-    origin: 'https://vagent.onrender.com', // Replace with your frontend's URL
+    origin: ['https://vagent.onrender.com','https://vagent.onrender.com/execute'], // Replace with your frontend's URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
