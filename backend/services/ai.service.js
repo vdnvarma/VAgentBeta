b9afdc29b93b34f72d8f149c77dc785b4d989a75
vdnvarma <dedeepn23@gmail.com>
2025-10-16 18:35:18 +0530

    updated gemini model

--- backend/services/ai.service.js
+++ backend/services/ai.service.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai"
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY);
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.5-flash",
     generationConfig: {
         responseMimeType: "application/json",
         temperature: 0.4,
